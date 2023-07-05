56cfc56dac99fedd5aee4c9305ad767ba6c6404c
5566b <106473242+5566b@users.noreply.github.com>
2023-07-05 19:07:20 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿var t = new List<int>
+{
+
+    1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+
+};
+var result = t.Any(a => == 1);
+   
+//foreach(var y  in result)
+//{
+
+//    Console.WriteLine(y);
+
+//}
+
+//t.Where(x => x * 5 == 12);
